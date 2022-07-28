22f5d70ffd68ecdbdda5bde28f8831532f732bf9
Chris <chris.baumgartner115@gmail.com>
2022-07-28 17:27:01 +0200

    javafx set up

--- Application/GradeManagementApplication.java
+++ Application/GradeManagementApplication.java
@@ -1,7 +1,27 @@
 package Application;
 
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 public class GradeManagementApplication extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane root = new BorderPane();
+        Label heading = new Label("Grade Management System");
+        root.setTop(heading);
+//        BorderPane.setAlignment(heading, Pos.CENTER);
 
+        primaryStage.setScene(new Scene(root, 300, 150));
+        primaryStage.setTitle("Grade Management System");
+        primaryStage.show();
+    }
 }
