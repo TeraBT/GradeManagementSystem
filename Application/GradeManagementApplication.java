586faa909f47a236daade83a40bf7f2faec41c95
Chris <chris.baumgartner115@gmail.com>
2022-07-30 19:02:52 +0200

    javafx small progress

--- Application/GradeManagementApplication.java
+++ Application/GradeManagementApplication.java
@@ -1,10 +1,13 @@
 package Application;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class GradeManagementApplication extends Application {
@@ -16,11 +19,16 @@ public class GradeManagementApplication extends Application {
     @Override
     public void start(Stage primaryStage) {
         BorderPane root = new BorderPane();
-        Label heading = new Label("Grade Management System");
-        root.setTop(heading);
-//        BorderPane.setAlignment(heading, Pos.CENTER);
+        HBox options = new HBox();
+        Button addModuleButton = new Button("add module");
+        Button showAveragesButton = new Button ("show averages");
+        options.getChildren().addAll(addModuleButton, showAveragesButton);
 
-        primaryStage.setScene(new Scene(root, 300, 150));
+        root.setTop(options);
+        root.setPadding(new Insets(5.));
+        HBox.setMargin(options, new Insets(10., 2., 2.,2.));
+
+        primaryStage.setScene(new Scene(root, 600, 300));
         primaryStage.setTitle("Grade Management System");
         primaryStage.show();
     }
