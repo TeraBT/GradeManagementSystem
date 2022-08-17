27b834e5b065b3d04c489ca041ebee222e35781e
Chris <chris.baumgartner115@gmail.com>
2022-08-17 12:32:43 +0200

    javafx studying & small progress

--- Application/StudyFX.java
+++ Application/StudyFX.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -13,9 +14,12 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class StudyFX extends Application{
 
     public static void main(String[] args) {
+        GradeManagementSystem gms;
         launch(args);
     }
 
@@ -24,29 +28,41 @@ public class StudyFX extends Application{
     @Override
     public void start(Stage primaryStage) {
 
+        GridPane startUpPane = new GridPane();
+        Button supButton = new Button("Load modules");
+        startUpPane.addRow(0, supButton);
+        int i = 0;
+        supButton.setOnAction(e -> {
+                try {
+                    DataManager.loadStudyPrograms();
+                } catch (IOException ex) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR, "Loading failed.");
+                    alert.show();
+                }
+        });
+        Scene startUp = new Scene(startUpPane, 600, 400);
+
+
 
 
         Label SPLabel1 = new Label("Study Program");
         TextField SPText1  = new TextField();
         Button SPButton1 = new Button("add study program");
 
-
-
-
         GridPane SPGridPane = new GridPane();
         SPGridPane.setHgap(10.);
         SPGridPane.setVgap(5.);
         SPGridPane.addRow(0, SPLabel1, SPText1, SPButton1);
-
-//        GridPane SP
 
         GridPane root = new GridPane();
         root.setHgap(20.);
         root.setVgap(20.);
         root.addRow(0, SPGridPane);
 
-        Scene scene = new Scene(root, 600, 400);
-        primaryStage.setScene(scene);
+        Scene sceneMain = new Scene(root, 600, 400);
+
+
+        primaryStage.setScene(startUp);
         primaryStage.setTitle("Grade Management System");
         primaryStage.show();
     }
